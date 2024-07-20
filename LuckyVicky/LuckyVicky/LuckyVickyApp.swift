5763cd934ff37cc865922fc9539581ad4f68dbc8
ye-gang <yegang13209@naver.com>
2024-07-20 16:29:41 +0900

    파일 생성

---
+++ LuckyVicky/LuckyVicky/LuckyVickyApp.swift
@@ -0,0 +1,17 @@
+//
+//  LuckyVickyApp.swift
+//  LuckyVicky
+//
+//  Created by yegang on 7/20/24.
+//
+
+import SwiftUI
+
+@main
+struct LuckyVickyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
